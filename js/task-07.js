2a91685eab869dc3b4c3e0ce08f376ee7bed5e81
Sergii Bogush <sergbogush@gmail.com>
2021-04-22 20:32:33 +0300

    1

--- js/task-07.js
+++
@@ -1,8 +0,0 @@
-const textRef = document.querySelector('#text');
-const inputRef = document.querySelector('#font-size-control');
-
-inputRef.addEventListener('input', rangeInput);
-
-function rangeInput(event) {
-  textRef.style.fontSize = event.currentTarget.value + 'px';
-}
