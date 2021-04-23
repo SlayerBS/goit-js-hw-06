193d2c90a5b3451d8ee7b12e748ae51d7d201914
Sergii Bogush <sergbogush@gmail.com>
2021-04-23 19:53:26 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -6,8 +6,10 @@ console.log(`В списке ${quantityOfCategories} категории`);
 const elemByTagH2 = document.querySelectorAll('li.item h2');
 
 navItemsEl.forEach(elem => {
-  const elemByTagH2 = document.querySelector('h2');
-  console.log(`Категория: ${elemByTagH2.textContent}`);
+  //const elemByTagH2 = document.querySelector('h2');
+  const nameOfCategory = elem.querySelector('h2').textContent;
+
+  console.log(`Категория: ${elem.querySelector('h2').textContent}`);
   let navLinksEl = elem.querySelectorAll('.item li');
   const quantityOfLinks = navLinksEl.length;
   console.log(`Количество елементов: ${quantityOfLinks}`);
